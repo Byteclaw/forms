aefae3ce9cbbe022b67467f32f41cb09f0ea22e3
Michal Kvasničák <michal.kvasnicak@gmail.com>
2018-12-18 11:34:41 +0100

    support react native onChange out of the box

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -21,45 +21,48 @@ interface IProps {
   [key: string]: any;
 }
 
-const Field = forwardRef(({
-  as = 'input',
-  children = null,
-  debounceDelay = 300,
-  name,
-  ...rest
-}: IProps, ref) => {
-  const parentField = useParentField();
-  const field = connectToParentField(name, parentField, useField, {
-    debounceDelay,
-  });
+const Field = forwardRef(
+  ({ as = 'input', children = null, debounceDelay = 300, name, ...rest }: IProps, ref) => {
+    const parentField = useParentField();
+    const field = connectToParentField(name, parentField, useField, {
+      debounceDelay,
+    });
 
-  if (typeof children === 'function') {
-    return (children as FieldRenderFn)(field) as ReactElement<any> | null;
-  }
+    if (typeof children === 'function') {
+      return (children as FieldRenderFn)(field) as ReactElement<any> | null;
+    }
 
-  function onBlur() {
-    field.setFocused(false);
-  }
+    function onBlur() {
+      field.setFocused(false);
+    }
 
-  function onFocus() {
-    field.setFocused(true);
-  }
+    function onFocus() {
+      field.setFocused(true);
+    }
 
-  function onChange(e: SyntheticEvent<HTMLInputElement>) {
-    field.setValue(e.currentTarget.value);
-  }
+    function onChange(e: SyntheticEvent<HTMLInputElement>) {
+      // react-native textInput (out of the box)
+      // @ts-ignore
+      if (e.nativeEvent && e.nativeEvent.text !== undefined) {
+        // @ts-ignore
+        field.setValue(e.nativeEvent.text);
+      } else if (e.currentTarget != null) {
+        field.setValue(e.currentTarget.value);
+      }
+    }
 
-  return createElement(as, {
-    ...rest,
-    children,
-    name,
-    onBlur,
-    onChange,
-    onFocus,
-    ref,
-    value: field.value,
-  });
-});
+    return createElement(as, {
+      ...rest,
+      children,
+      name,
+      onBlur,
+      onChange,
+      onFocus,
+      ref,
+      value: field.value,
+    });
+  },
+);
 
 Field.displayName = 'Field';
 
