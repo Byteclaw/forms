deafb351e8a56f39b41685bb5578c56ae3bfe0e5
Michal Kvasničák <michal.kvasnicak@gmail.com>
2019-02-27 17:12:19 +0100

    use own simple mock to mock debounce

--- jest/setup.ts
+++ jest/setup.ts
@@ -5,6 +5,20 @@ global.Promise = P;
 
 jest.useFakeTimers();
 
+jest.mock('lodash.debounce', () => (cb, delay) => {
+  let tmt;
+  const debounce = value => {
+    clearTimeout(tmt);
+    tmt = setTimeout(cb, delay, value);
+  };
+
+  debounce.cancel = () => {
+    tmt = clearTimeout(tmt);
+  };
+
+  return debounce;
+});
+
 // remove all scheduled timers so they are not fired outside of tests
 // where they were scheduled
 afterEach(() => jest.clearAllTimers());
