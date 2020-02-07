20c50e4aa31016f0590bdff3ed6a804be7be62ac
Michal Kvasničák <michal.kvasnicak@gmail.com>
2020-02-07 18:38:00 +0100

    fix mapping nested yup validation errors

--- src/utils/createValidatorFromYup.ts
+++ src/utils/createValidatorFromYup.ts
@@ -1,6 +1,10 @@
 import * as yup from 'yup';
 import { ValidationError } from '../hooks';
 
+function splitPath(path: string): string[] {
+  return path.replace(/[[\]]/g, '.').split(/\.+/);
+}
+
 export function createValidatorFromYup<
   TDefaultValue extends { [key: string]: any } = { [key: string]: any }
 >(schema: yup.Schema<TDefaultValue>, options?: yup.ValidateOptions) {
@@ -10,9 +14,9 @@ export function createValidatorFromYup<
     return schema.validate(value, options).catch(e => {
       if (yup.ValidationError.isError(e)) {
         throw new ValidationError([
-          { path: e.path ? e.path.split('.') : [], error: e.message },
+          { path: e.path ? splitPath(e.path) : [], error: e.message },
           ...e.inner.map(innerError => ({
-            path: innerError.path ? innerError.path.split('.') : [],
+            path: innerError.path ? splitPath(innerError.path) : [],
             error: innerError.message,
           })),
         ]);
